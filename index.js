44bb4aa81464e04a0d416d7d784bf350ec5e61fd
mohit <mohityadavkkn24@gmail.com>
2023-08-12 17:59:42 +0530

    app connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+
 const express=require('express');
+const mongoose=require('mongoose');
 const port =800;
 const app=express();
+
+//app connection to mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/doctor_api',{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=> {
+    console.log("App is now connected to DB")
+}).catch((err)=> {
+    console.log(`${err}`);
+})
+
 
 app.get('/',(req,res)=>{
        res.send('hello');
 })
 
+
+// app listening to port
 app.listen(port,(err)=>{
     if(err){
         console.log(err);
